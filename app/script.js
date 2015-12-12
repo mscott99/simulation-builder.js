2795496621508c31d407a760d333de9a4943019d
Matthew Scott <Matthew Scott>
2015-12-11 21:04:44 -0500

    first actual 3d!

--- app/script.js
+++ app/script.js
@@ -14,28 +14,27 @@ var base ={
 		base.line(0,canvas.clientHeight/2,canvas.clientWidth,canvas.clientWidth/2);
 	},
 	point: function(x,y){
-		drawing.fillRect(x,y,1,1);
+		drawing.fillRect(x+canvas.clientWidth/2,y+canvas.clientHeight/2,1,1);
 	},
+	
 	line  :  function(originX,originY,goalX,goalY){
 	
-			drawing.moveTo(originX,originY);
-			drawing.lineTo(goalX,goalY);
+			drawing.moveTo(originX+canvas.clientWidth/2,originY+canvas.clientHeight/2);
+			drawing.lineTo(goalX+canvas.clientWidth/2,goalY+canvas.clientHeight/2);
 			drawing.stroke();
 		},
 	
 	drawLine : function(point1,point2){
-		drawing.moveTo(point1.x,point1.y);
-			drawing.lineTo(point2.x,point2.y);
+		drawing.moveTo(point1.x+canvas.clientWidth/2,point1.y+canvas.clientHeight/2);
+			drawing.lineTo(point2.x+canvas.clientWidth/2,point2.y+canvas.clientHeight/2);
 			drawing.stroke();
 	},
 	drawGivenLine : function(line){
-		if(line.isConverted){
+		
 		drawing.moveTo(line.point1.x,line.point1.y);
 			drawing.lineTo(line.point2.x,line.point2.y);
 			drawing.stroke();
-		}else{
-			base.line(0,0,1000,1000);
-		}
+		
 	},
 	simpleGrid : function(originX,originY,goalX,goalY,numLine,space){
 		var x;
@@ -75,20 +74,20 @@ function Point(x,y,z){
 	this.x = x;
 	this.y = y;
 	this.z = z;
-	this.isConverted = false;
+	
 }
 
 Point.prototype.convert= function(){
 	return{
-		x:this.x*canvas.clientWidth/(this.z+canvas.clientWidth)+canvas.clientWidth/2,
-		y:this.y*canvas.clientHeight/(this.z+canvas.clientHeight)+canvas.clientHeight/2,
+		x:(this.x*canvas.clientWidth)/(this.z+canvas.clientWidth),
+		y:(this.y*canvas.clientHeight)/(this.z+canvas.clientHeight),
 		isConverted:true
 		}
 }
 
 //this is used when we only want to draw one point: not very often
 Point.prototype.draw = function(){
-	base.point(this.x*canvas.clientWidth/(this.z+canvas.clientWidth)+canvas.clientWidth/2,this.y*canvas.clientHeight/(this.z+canvas.clientHeight)+canvas.clientHeight/2);
+	base.point((this.x*canvas.clientWidth)/(this.z+canvas.clientWidth),(this.y*canvas.clientHeight)/(this.z+canvas.clientHeight));
 }
 //this function can take in two points or four coordinates. If called in two points,  enter twoPoints as true
 
@@ -102,7 +101,7 @@ function Line(x1,y1,z1,x2,y2,z2, fromTwoPoints){
 	this.point1 = new Point(x1,y1,z1);
 	this.point2 = new Point(x2,y2,z2);
 }
-	this.isConverted = false;
+	
 }
 
 Line.prototype.convert= function(){
@@ -121,28 +120,69 @@ Line.prototype.draw = function(){
 }
 //point1 is linked to point 2 which is linked to point 3
 function Grid(point1,point2,point3, linesBetweenPoint1_Point2, linesBetweenPoint2_Point3){
-	this.mainPoints=[point1,point2,point3];
-	//this line makes a fictionnal center point of the figure, then takes the difference with point 2 and adds it on to its own value(of the center point)
-	this.mainPoints[3] = new Point(point1.x+point3.x-point2.x,point1.y+point3.y-point2.y, point1.z+point3.z-point2.z);
-	
-	var order =[[0,1][3,2][3,0][2,1]];
-	for(var x=0; x<4;x++){
-		var origin = order[x][0];
-		var goal = order[x][1];
-	var coefficientx = (this.mainPoints[goal].x-this.mainPoints[origin].x)/(linesBetweenPoint1_Point2+1);
-	var coefficienty = (this.mainPoints[goal].y-this.mainPoints[origin].y)/(linesBetweenPoint1_Point2+1);
-	var coefficientz = (this.mainPoints[goal].z-this.mainPoints[origin].z)/(linesBetweenPoint1_Point2+1);
-	if(x==0 || x==1){
-		var numLines = linesBetweenPoint1_Point2;
-	}else{
-		var numLines = linesBetweenPoint2_Point3;
-	}
-	for(var l=0;l<numLines;l++){
-	this.minorPoints[x][l]=new Point(coefficientx*(l+1),coefficienty*(l+1), coefficientz*(l+1));
-	}
+		this.minorPoints = [[0],[0],[0],[0]];//to create array of arrays
+		this.mainPoints=[point1,point2,point3];
+		
+		//this line makes a fictionnal center point of the figure, then takes the difference with point 2 and adds it on to its own value(of the center point)
+		this.mainPoints[3] = new Point(point1.x+point3.x-point2.x,point1.y+point3.y-point2.y, point1.z+point3.z-point2.z);
+		
+		var order =[[0,1],[3,2],[3,0],[2,1]];
+		
+		for(var x=0; x<4;x++){
+			
+			var origin = order[x][0];
+			var goal = order[x][1];
+			
+			if(x==0 || x==1){
+				var numLines = linesBetweenPoint1_Point2;
+			}else{
+				var numLines = linesBetweenPoint2_Point3;
+			}
+			
+			var coefficientx = (this.mainPoints[goal].x-this.mainPoints[origin].x)/(numLines+1);
+			var coefficienty = (this.mainPoints[goal].y-this.mainPoints[origin].y)/(numLines+1);
+			var coefficientz = (this.mainPoints[goal].z-this.mainPoints[origin].z)/(numLines+1);
+			
+			
+			 
+			for(var l=0;l<numLines+2;l++){
+				
+				
+				this.minorPoints[x][l]=new Point(this.mainPoints[origin].x+coefficientx*l,this.mainPoints[origin].y+coefficienty*l, this.mainPoints[origin].z+coefficientz*l);
+				this.minorPoints[x][l].draw();
+			
+				
+			}
+		
+			this.mainPoints[x].draw();
+			
+		}
+		
+		//creation of points finished
+		//creation of lines start
+		this.lines=[[0],[0]];
+		for(var m=0;m<linesBetweenPoint1_Point2+2;m++){
+			this.lines[0][m] = new Line(this.minorPoints[0][m],this.minorPoints[1][m],0,0,0,0,true);
+			this.lines[0][m].draw();
+		}
+		
+		for(var m=0;m< linesBetweenPoint2_Point3 +2;m++){
+			this.lines[1][m] = new Line(this.minorPoints[2][m],this.minorPoints[3][m],0,0,0,0,true);
+			this.lines[1][m].draw();
+		}
+		
+		
+		
+		
 }
-}
-base.inputScale();
+var point1 = new Point(100,200,0);
+var point2 = new Point(-100,200,0);
+var point3 = new Point(-100,200,700);
+var line = new Line(point1,point2,0,0,0,0,true);
+
+
+var grid1 = new Grid(point1,point2,point3,5,7);
+
 
 
 
